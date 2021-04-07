98741b0aee285c1eb53a245e5e957eb6888ca4a3
rodri595 <rodri.595@hotmail.com>
2021-04-07 16:40:41 -0600

    try

--- src/pages/Contact/Axios.js
+++ src/pages/Contact/Axios.js
@@ -8,13 +8,16 @@ export const emailContact = async (
   fromwhere
 ) => {
   try {
-    const { data } = await naxios.post("/api/email/", {
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-      fromwhere: fromwhere,
-    });
+    const { data } = await naxios.post(
+      "https://rodriscerebro.herokuapp.com/api/email/",
+      {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+        fromwhere: fromwhere,
+      }
+    );
     return data;
   } catch (e) {
     throw e;
